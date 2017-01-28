76ac15785dc9b5da4f054d1cc3170203916edece
John Chen <john@johnchen.co>
2017-01-28 14:36:44 -0500

    webpack

---
+++ webpack.dist.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  context: __dirname + '/src',
+  entry: 'index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/assets/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  }
+  devtool: 'source-map',
+  
+}
