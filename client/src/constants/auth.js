853ae4c5600ddb4c97ffe10782a98426c0fac2f8
John Chen <john@johnchen.co>
2017-02-01 00:07:47 -0500

    loginform

--- client/src/constants/auth.js
+++ client/src/constants/auth.js
@@ -1,7 +1,7 @@
-export const REQUEST_LOGIN = 'REQUEST_LOGIN';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 
-export const REQUEST_SIGN_UP = 'REQUEST_SIGN_UP';
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILED = 'SIGN_UP_FAILED';
